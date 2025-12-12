9ed90fde2188adfb91b0f58005e1be8231bf295e
SilvanaPSilva <s00273672@atu.ie>
2025-12-12 13:41:01 +0000

    Loggin Error
    Pop up "Loggin error" working

--- public/custom-js/login.js
+++ public/custom-js/login.js
@@ -1,25 +1,30 @@
 console.log("login.js loaded");
 
-document.getElementById('user-login').addEventListener('submit',LoginSubmit);
+document.getElementById('user-login').addEventListener('submit', LoginSubmit);
 
 function LoginSubmit(event) {
+    event.preventDefault();
     var email = document.getElementById('emailAddressID').value;
-    var password= document.getElementById('passwordID').value;
-    if (email==="your_email_here@email.com" && password==="12345678") {   
+    var password = document.getElementById('passwordID').value;
+    if (email === "your_email_here@email.com" && password === "12345678") {
         // successful login, user redirected to shop.html
-        localStorage.setItem('loggedIn',"1");    
-        window.location.href = "shop.html";  // redirect to shop page
+        localStorage.setItem('loggedIn', "1"); // set loggedIn to 1 (true)
+        //loadNode();
+        window.location.href = "/buyIt";  // redirect to shop page (buyIt)
     }
     else {
-        alert("invalid login details");
+        alert("Invalid login details.\n The username or password is incorrect.");
         // login unsuccessful, error message appears
-        localStorage.setItem('loggedIn',0);
+        localStorage.setItem('loggedIn', '0'); // set loggedIn to 0 (false)
         var element = document.getElementById("loginerror");
-       // element.classList.remove("d-none");
-       // element.classList.remove("d-block");
+        // element.classList.remove("d-none");
+        // element.classList.remove("d-block");
     }
-    event.preventDefault();
-  };
+    
+    //> <a href="/login">Forgot
+                //   your
+                //   password?</a>
+};
 
 
 
@@ -31,28 +36,26 @@ function LoginSubmit(event) {
 
 
 
+/* document.getElementById('user-login').addEventListener('submit', (event) => {
+// wait for submit button to be clicked on login form - 
+  // this code only invoked if login form submit button clicked
 
-
-  /* document.getElementById('user-login').addEventListener('submit', (event) => {
-  // wait for submit button to be clicked on login form - 
-    // this code only invoked if login form submit button clicked
-
-    var email = document.getElementById('emailAddressID').value;
-    var password= document.getElementById('passwordID').value;
-    if (email==="your_email_here@email.com" && password==="12345678") {   
-        // successful login, user redirected to shop.html
-        localStorage.setItem('loggedIn',1);    
-        window.location.href = "shop.html";  // redirect to shop page
-    }
-    else {
-        alert("invalid login details");
-        // login unsuccessful, error message appears
-        localStorage.setItem('loggedIn',0);
-        var element = document.getElementById("loginerror");
-       // element.classList.remove("d-none");
-       // element.classList.remove("d-block");
-    }
-    event.preventDefault();
-  }); */
+  var email = document.getElementById('emailAddressID').value;
+  var password= document.getElementById('passwordID').value;
+  if (email==="your_email_here@email.com" && password==="12345678") {   
+      // successful login, user redirected to shop.html
+      localStorage.setItem('loggedIn',1);    
+      window.location.href = "shop.html";  // redirect to shop page
+  }
+  else {
+      alert("invalid login details");
+      // login unsuccessful, error message appears
+      localStorage.setItem('loggedIn',0);
+      var element = document.getElementById("loginerror");
+     // element.classList.remove("d-none");
+     // element.classList.remove("d-block");
+  }
+  event.preventDefault();
+}); */
 
 
